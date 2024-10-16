79294f03d9d549bfdc860cc9c9c9fc92a6432de2
xiaoshae(linux) <xiaoshae@gmail.com>
2024-10-16 13:48:08 +0800

    python parse_header

---
+++ python/parse_header.py
@@ -0,0 +1,36 @@
+import json
+import re
+
+
+def parse_header(raw_header: str , msg : bool = False ):
+
+    dict_header = dict()
+
+    for line in raw_header.split("\n"):
+
+        # 跳过空行或纯注释行（如果有的话）
+        if not re.match(r'^(.*?):\s', line) :
+
+            if msg : # msg == True
+                print('parse_header warnMsg: "' + line + '" Format Error')
+
+            continue
+
+        # 拆分键值对
+        key, value = line.split(":", 1)
+
+        # 去除 键值对 的空格
+        dict_header[ key.strip() ] = value.strip()
+
+    return dict_header
+
+header = """GET / HTTP/1.1
+Host: 127.0.0.1:8080
+Upgrade-Insecure-Requests: 1
+Accept-Language: zh-CN,zh;
+Cache-Control: no-cache
+Connection: keep-alive
+Pragma: no-cache
+Accept-Encoding: gzip, deflate"""
+
+print(json.dumps(parse_header(header),indent=4))
