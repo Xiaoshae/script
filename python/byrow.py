1cb14c769c4eb93ac7e7ce89ee57ba3805c5aa2f
xiaoshae(linux) <xiaoshae@gmail.com>
2024-10-18 17:46:45 +0800

    python byrow.py 将字符串按分隔符拆分后 按行存放

---
+++ python/byrow.py
@@ -0,0 +1,59 @@
+def byrow(file_path : str , delimiter : str = None):
+
+    result = []
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+
+    for line in lines:
+        # 去除行末的换行符并按空格分割
+        items = line.strip().split(delimiter)
+        for item in items:
+            # item 字符串 不为空
+            if item :
+                result.append(item)
+
+    return result
+
+
+
+input_file = r'./test/byrow/iconv-line.txt'
+output_file = r"./test/byrow/iconv-list.txt"
+
+#参数1：要操作的文件名
+#参数2：分割符（字符串） 默认为：none  按照多个空白字符分割
+result = byrow(input_file)
+
+with open(output_file, 'w', encoding='utf-8') as output_file:
+    for item in result:
+        output_file.write(f"{item}\n")
+
+"""
+输入示例：
+ANSI_X3.4-1968 ANSI_X3.4-1986 ASCII CP367 IBM367 ISO-IR-6 ISO646-US ISO_646.IRV:1991 US US-ASCII CSASCII
+UTF-8
+ISO-10646-UCS-2 UCS-2 CSUNICODE
+UCS-2BE UNICODE-1-1 UNICODEBIG CSUNICODE11
+
+输出结果：
+ANSI_X3.4-1968
+ANSI_X3.4-1986
+ASCII
+CP367
+IBM367
+ISO-IR-6
+ISO646-US
+ISO_646.IRV:1991
+US
+US-ASCII
+CSASCII
+UTF-8
+ISO-10646-UCS-2
+UCS-2
+CSUNICODE
+UCS-2BE
+UNICODE-1-1
+UNICODEBIG
+CSUNICODE11
+"""
